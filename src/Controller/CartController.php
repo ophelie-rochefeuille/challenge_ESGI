6b0d88346073e9d56feface8a1814820a258b3a1
Ophelie Rochefeuille <ophelie.rochefeuille@green-conseil.com>
2023-02-10 16:11:57 +0100

    Payment: initialize Stripe API

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -5,13 +5,18 @@ namespace App\Controller;
 use App\Entity\Art;
 use App\Repository\ArtRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Stripe;
+
 
 #[Route('/buyer/cart')]
 class CartController extends AbstractController
 {
+
+
     #[Route('/', name: 'buyer_cart_index')]
     public function index(SessionInterface $session, ArtRepository $artRepository): Response
     {
@@ -61,5 +66,45 @@ class CartController extends AbstractController
 
         return $this->redirectToRoute("buyer_cart_index");
 
+    }
+
+    // payment's functions
+
+    #[Route('/stripe', name: 'app_stripe')]
+    public function stripe(): Response
+    {
+        return $this->render('stripe/index.html.twig', [
+            'stripe_key' => $_ENV["STRIPE_KEY"],
+        ]);
+    }
+
+    #[Route('/stripe/create-payment', name: 'app_stripe_payment', methods: ['POST'])]
+    public function createPayment(Request $request, SessionInterface $session, ArtRepository $artRepository)
+    {
+        $cart = $session->get("cart", []);
+        $dataCart= [];
+        $total = 0;
+
+        foreach ($cart as $id => $quantity){
+            $art = $artRepository->find($id);
+            $dataCart[] = [
+                "oeuvre" => $art,
+                "quantity" => $quantity
+            ];
+            $total += $art->getPrice() * $quantity;
+        };
+
+        Stripe\Stripe::setApiKey($_ENV["STRIPE_SECRET"]);
+        Stripe\Charge::create ([
+            "amount" => $total,
+            "currency" => "eur",
+            "source" => $request->request->get('stripeToken'),
+            "description" => "Binaryboxtuts Payment Test"
+        ]);
+        $this->addFlash(
+            'success',
+            "` Paiement validé : {$total} € au total`"
+        );
+        return $this->redirectToRoute('app_stripe', [], Response::HTTP_SEE_OTHER);
     }
 }
